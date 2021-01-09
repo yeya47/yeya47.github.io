355621293931b5018fdd1cdce3dff7ee56c86125
yeya47 <jua20002@byui.edu>
2021-01-09 16:01:49 -0600

    updat

---
+++ js/currentdate.js
@@ -0,0 +1,3 @@
+
+const options = {weekday:"long", day: "numeric", month:"long",year:"numeric"};
+document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en.us", options);
