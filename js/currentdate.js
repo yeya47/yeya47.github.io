70d6cc864b9d3dd86d6fb042a531bee84102dfc4
Yeya47 <jua20002@byui.edu>
2021-01-21 22:29:30 -0600

    sav

--- js/currentdate.js
+++ js/currentdate.js
@@ -32,9 +32,9 @@ document.getElementById("currentdate").textContent = fulldate;
 try {
   let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
   document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
-//*} catch (e) {
- //* alert('Error with code or your browser does not support Locale');
-    }}
+} catch (e) {
+  alert('Error with code or your browser does not support Locale');
+}
 
 // problme with toLocaleDateString order ... 
 
