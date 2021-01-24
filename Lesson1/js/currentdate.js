d769b2ca7fe42da840940070a0201a5152d6617b
Yeya47 <jua20002@byui.edu>
2021-01-23 18:08:00 -0600

    chang

--- Lesson1/js/currentdate.js
+++ Lesson1/js/currentdate.js
@@ -1,40 +1,6 @@
-let daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-let months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-let d = new Date();
-let dayName = daynames[d.getDay()];
-let monthName = months[d.getMonth()];
-let year = d.getFullYear();
-//const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
-let fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-document.getElementById("currentdate").textContent = fulldate;
-// ****************************
-try {
-  let options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
-} catch (e) {
-  alert('Error with code or your browser does not support Locale');
+function dateTime(){
+	let dateNow = document.lastModified;
+	document.getElementById("currentdate").innerHTML= dateNow;
+	let newYear = new Date().getFullYear();
+	document.getElementById("year").innerHTML= newYear;
 }
-
-// problme with toLocaleDateString order ... 
-
