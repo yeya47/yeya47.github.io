857a53d55d74daa299e889f47ffeab327369cc7a
Yeya47 <jua20002@byui.edu>
2021-01-23 18:18:13 -0600

    chnag

--- Lesson1/js/currentdate.js
+++ Lesson1/js/currentdate.js
@@ -1,6 +1,13 @@
-function dateTime(){
-	let dateNow = document.lastModified;
-	document.getElementById("currentdate").innerHTML= dateNow;
-	let newYear = new Date().getFullYear();
-	document.getElementById("year").innerHTML= newYear;
-}
+//*function dateTime(){
+//*	let dateNow = document.lastModified;
+//*	document.getElementById("currentdate").innerHTML= dateNow;
+//*	let newYear = new Date().getFullYear();
+//*	document.getElementById("year").innerHTML= newYear;
+//}
+new Date().getFullYear()
+ document.getElementById("year").textContent = new Date().getFullYear();
+ document.getElementById("date").textContent = document.lastModified;
+
+
+
+
