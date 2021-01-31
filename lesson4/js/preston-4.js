359ac4f04e6389e00e756bffdbc858d10cb77749
Yeya47 <jua20002@byui.edu>
2021-01-30 21:05:54 -0600

    Push

--- lesson4/js/preston-4.js
+++ lesson4/js/preston-4.js
@@ -1,6 +1,16 @@
-function dateTime(){
-	let dateNow = document.lastModified;
+//*function dateTime(){
+/*	let dateNow = document.lastModified;
 	document.getElementById("currentdate").innerHTML= dateNow;
 	let newYear = new Date().getFullYear();
 	document.getElementById("year").innerHTML= newYear;
+}
+*/
+
+
+//*JS for Nav Functions/*
+
+
+function toggleMenu(){
+	
+	document.getElementById("primeNav").classList.toggle("hide");
 }
