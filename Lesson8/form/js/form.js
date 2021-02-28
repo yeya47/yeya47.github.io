dc67908e0d411b2f5ff46d2a6881ce54a5438732
Yeya47 <jua20002@byui.edu>
2021-02-27 19:13:44 -0600

    update

---
+++ Lesson8/form/js/form.js
@@ -0,0 +1,13 @@
+//*Select Input function*//
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
+
+//* Storm Raiting*//
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
