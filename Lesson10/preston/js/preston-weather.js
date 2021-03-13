740c896734432f93880881caa2ee1fb89ddb1a39
Yeya47 <jua20002@byui.edu>
2021-03-13 17:17:26 -0600

    change

--- Lesson10/preston/js/preston-weather.js
+++ Lesson10/preston/js/preston-weather.js
@@ -15,10 +15,10 @@ fetch(apiURL)
   const humidityprcnt = document.querySelector('#humidity');
   humidityprcnt.textContent = jsObject.main.humidity;
   //Get image
-  const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`; // note the concatenation
+  /* const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`; // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
-windChill();
+windChill();*/
   }); 
