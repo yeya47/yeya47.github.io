28473f3eed488f1f1c137c69bd6ed0e9da3e6adb
Yeya47 <jua20002@byui.edu>
2021-03-13 12:08:49 -0600

    update

--- Lesson10/preston/js/forecast.js
+++ Lesson10/preston/js/forecast.js
@@ -1,7 +1,9 @@
 const apiURL= 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=6b7db5f6c8ffac4df697298da2f5a480&units=imperial'
+
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-    windChill();
+  console.log(jsObject);
+  //Get Temp located in JSON file inside the main
+
   });
