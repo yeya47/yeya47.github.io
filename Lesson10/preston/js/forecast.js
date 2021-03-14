a0db8b3f9a46a10d921f10f5caa490ad7d78b6f4
Yeya47 <jua20002@byui.edu>
2021-03-13 18:39:16 -0600

    Change

--- Lesson10/preston/js/forecast.js
+++ Lesson10/preston/js/forecast.js
@@ -1,6 +1,6 @@
-const apiURL= 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=6b7db5f6c8ffac4df697298da2f5a480&units=imperial';
+const apiURL2= 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=6b7db5f6c8ffac4df697298da2f5a480&units=imperial';
 
-fetch(apiURL)
+fetch(apiURL2)
   .then((response) => response.json())
   .then((jsObject) => {
   console.log(jsObject);
