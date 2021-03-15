91c04850177096fdd568ec8b345b85e191ac960c
Yeya47 <jua20002@byui.edu>
2021-03-14 22:35:04 -0500

    update

--- Lesson10/preston/js/forecast.js
+++ Lesson10/preston/js/forecast.js
@@ -3,20 +3,23 @@ const apiURL2= 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appi
 fetch(apiURL2)
   .then((response) => response.json())
   .then((jsObject) => {
-  console.log(jsObject);
+  //console.log(jsObject);
   //Get Temp located in JSON file inside the main
    let day = 0;
-   const dayoW = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+  const dayoW = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
    //reduce list to 5 .. filter 
    // filter take array jsObject.list.filter 
-   //feed filter condition ex: 'string.includes('comdition1800'
+   //feed filter condition ex: 'string.includes('condition1800'
    //loop forecast
-  // const five = jsObject.list.filter(list.dt_txt== '18:00:00');
-  // five.forEach(x=>{
-   let d = new Date(jsObject.list[4].dt_txt);
-   console.log(d);
+   const five = jsObject.list.filter((x) => x.dt_txt.includes ('18:00:00'));
+   console.log(five);
+   five.forEach(x=>{
+   let d = new Date(x.dt_txt);
+   console.log(x.dt_txt);
    document.getElementById(`dayoW${day+1}`).textContent= dayoW[d.getDay()];
-   document.getElementById(`ftemperature${day+1}`).textContent = jsObject.list[4].main.temp;
-   //d++
-// });
+   document.getElementById(`ftemperature${day+1}`).textContent = x.main.temp;
+   let icon = x.weather[0].icon;
+   document.getElementById(`icon${day+1}`).src =`https://openweathermap.org/img/wn/${icon}@2x.png`;
+   d++;
+ });
 });
