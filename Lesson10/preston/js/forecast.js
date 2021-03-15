5bf39ba6fc9143e7e3d82275c23a4c63fd3562ff
Yeya47 <jua20002@byui.edu>
2021-03-14 22:06:04 -0500

    chnage

--- Lesson10/preston/js/forecast.js
+++ Lesson10/preston/js/forecast.js
@@ -5,5 +5,18 @@ fetch(apiURL2)
   .then((jsObject) => {
   console.log(jsObject);
   //Get Temp located in JSON file inside the main
-
-  });
+   let day = 0;
+   const dayoW = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+   //reduce list to 5 .. filter 
+   // filter take array jsObject.list.filter 
+   //feed filter condition ex: 'string.includes('comdition1800'
+   //loop forecast
+  // const five = jsObject.list.filter(list.dt_txt== '18:00:00');
+  // five.forEach(x=>{
+   let d = new Date(jsObject.list[4].dt_txt);
+   console.log(d);
+   document.getElementById(`dayoW${day+1}`).textContent= dayoW[d.getDay()];
+   document.getElementById(`ftemperature${day+1}`).textContent = jsObject.list[4].main.temp;
+   //d++
+// });
+});
