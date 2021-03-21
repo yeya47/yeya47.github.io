d5503a6c040678e4314dc08b33ca2e9858416e98
Yeya47 <jua20002@byui.edu>
2021-03-20 21:22:04 -0500

    chnage

--- Lesson11/js/form.js
+++ Lesson11/js/form.js
@@ -1,7 +1,6 @@
 //*submit page link*//
-function myFunction() {
-    window.location.href="https://yeya47.github.io/Lesson8/form/thanks.html";
-  }//*Select Input function*//
+
+  //*Select Input function*//
 function selectResponse() {
 	const s = document.querySelector('#selected')
 	const sel = document.querySelector('#selectbrowser');
