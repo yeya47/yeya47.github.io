eaf5dc13551333b40400afa02be68662f1b10603
Yeya47 <jua20002@byui.edu>
2021-04-06 23:42:02 -0500

    chnage

--- Final/js/directoy.js
+++ Final/js/directoy.js
@@ -1,5 +1,5 @@
 //*Source
-const requestURL= 'https://byui-cit230.github.io/weather/data/towndata.json';
+const requestURL= 'https://yeya47.github.io/Final/json/spacecity.json';
 
 //* Fetch to obtain promise*//
 fetch(requestURL)
@@ -8,37 +8,35 @@ fetch(requestURL)
 })
 .then(function(jsonObject) {
 
-    const towns =jsonObject['towns'];
+    const business =jsonObject['business'];
     const cards = document.querySelector('.cards')
 
-    towns.forEach(town => {
-        if (town.name == 'Preston'|| town.name == 'Fish Haven'|| town.name == 'Soda Springs'){
+    business.forEach(business => {
+        if (business.name == 'EMG Center of Houston'|| business.name == 'Chevron Corporation'|| business.name == 'Prestige Auto & Metal Recycling'){
         let card = document.createElement('section');   
         let h2 = document.createElement('h2');
-        let motto = document.createElement('h3'); 
+        let phone = document.createElement('h3'); 
         let pimg = document.createElement('img');
-        let yearFounded = document.createElement('p'); 
-        let currentPopulation = document.createElement('p');
-        let averageRainfall = document.createElement('p');
+        let address= document.createElement('p'); 
+        let website = document.createElement('p');
+       
         
         //Tempalte literals*//
-        h2.textContent = `${town.name}`;
-        motto.textContent = `${town.motto}`;
-        yearFounded.textContent = `Year Founded: ${town.yearFounded}`;
-        currentPopulation.textContent = `Population: ${town.currentPopulation}`;
-        averageRainfall.textContent = `Annual Rain Fall: ${town.averageRainfall}`;
-        pimg.setAttribute('src',`images/${town.photo}`);
-        pimg.setAttribute('alt',`Photo of: ${town.name}`)
+        h2.textContent = `${business.name}`;
+        phone.textContent = `${business.phone}`;
+        address.textContent = `${business.address}`;
+        website.textContent = ` ${business.website}`;
+        pimg.setAttribute('src',`images/${business.photo}`);
+        pimg.setAttribute('alt',`Photo of: ${business.name}`)
  
 
 
         //*Card Append//
         
      card.append(h2);
-     card.appendChild(motto)
-     card.appendChild(yearFounded);
-     card.appendChild(currentPopulation);
-     card.appendChild(averageRainfall);
+     card.appendChild(phone)
+     card.appendChild(address);
+     card.appendChild(website);
      card.append(pimg)
      cards.append(card);
     }
